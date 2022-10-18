d42c2488cb21a554693f8a2210d3571871851e5a
DucHienIT <nguyenduc345345@gmail.com>
2022-10-18 21:49:29 +0700

    ngidia

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,13 +1,24 @@
 import classNames from 'classnames/bind';
-import styles from './Header.module.scss';
-import images from '~/assets/images';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
+import styles from './Header.module.scss';
+import images from '~/assets/images';
+import Tippy from '@tippyjs/react/headless';
+import { useEffect, useState } from 'react';
+//import 'tippy.js/dist/tippy.css'
 
 const cx = classNames.bind(styles);
 
 function Header() {
+    const [searchResult, setSearchResult] = useState([]);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setSearchResult([1, 2, 3]);
+        }, 3000);
+    }, []);
+
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
@@ -15,18 +26,33 @@ function Header() {
                     <img src={images.logo} alt="TikTok" />
                 </div>
 
-                <div className={cx('search')}>
-                    <input placeholder="Search accouts and videos " spellCheck={false}></input>
-                    <button className={cx('clear')}>
-                        <FontAwesomeIcon icon={faCircleXmark} />
-                    </button>
-                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                <Tippy
+                    interactive
+                    visible={searchResult.length > 0}
+                    render={(attrs) => (
+                        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                            KetQua
+                        </div>
+                    )}
+                >
+                    <div className={cx('search')}>
+                        <input placeholder="Search accouts and videos " spellCheck={false}></input>
+                        <button className={cx('clear')}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                        <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
 
-                    <button className={cx('search-btn')}>
+                        <button className={cx('search-btn')}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </button>
+                    </div>
+                </Tippy>
+                <div className={cx('action')}>
+                    <div>
                         <FontAwesomeIcon icon={faMagnifyingGlass} />
-                    </button>
+                        <button>Upload</button>
+                    </div>
                 </div>
-                <div className={cx('action')}></div>
             </div>
         </header>
     );
